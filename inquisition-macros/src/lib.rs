44ff424c0ecd190cce153133d7a562fb9dd70adc
Lutetium-Vanadium <luv.s7000@gmail.com>
2021-05-12 14:45:38 +0530

    default to vec for questions, but allow array

--- inquisition-macros/src/lib.rs
+++ inquisition-macros/src/lib.rs
@@ -4,13 +4,13 @@ use proc_macro::TokenStream;
 #[rustversion::since(1.51)]
 macro_rules! iter {
     ($span:expr => $($tt:tt)*) => {
-        quote! { ::std::array::IntoIter::new([ $($tt)* ]) }
+        quote::quote_spanned! { $span => ::std::array::IntoIter::new([ $($tt)* ]) }
     };
 }
 #[rustversion::before(1.51)]
 macro_rules! iter {
     ($span:expr => $($tt:tt)*) => {
-        quote! { ::std::vec![ $($tt)* ] }
+        quote::quote_spanned! { $span => ::std::vec![ $($tt)* ] }
     };
 }
 
@@ -18,7 +18,6 @@ mod helpers;
 mod question;
 
 use question::*;
-use quote::quote;
 use syn::{parse::Parse, parse_macro_input, Token};
 
 #[proc_macro]
@@ -27,18 +26,30 @@ pub fn questions(item: TokenStream) -> TokenStream {
 
     let questions = p.questions.into_iter();
 
-    let ts = iter! { proc_macro2::Span::call_site() => #(#questions),* };
-
-    ts.into()
+    if p.inline {
+        iter! { proc_macro2::Span::call_site() => #(#questions),* }
+    } else {
+        quote::quote! { ::std::vec![ #(#questions),* ] }
+    }
+    .into()
 }
 
 struct Questions {
+    inline: bool,
     questions: syn::punctuated::Punctuated<Question, Token![,]>,
 }
 
 impl Parse for Questions {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let inline = input
+            .step(|c| match c.ident() {
+                Some((ident, c)) if ident == "inline" => Ok(((), c)),
+                _ => Err(c.error("no inline")),
+            })
+            .is_ok();
+
         Ok(Self {
+            inline,
             questions: input.parse_terminated(Question::parse)?,
         })
     }
