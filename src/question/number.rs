d8905971cdd762408213e378781070de6106f015
Lutetium-Vanadium <luv.s7000@gmail.com>
2021-06-23 19:27:43 +0530

    added tests for int & float

--- src/question/number.rs
+++ src/question/number.rs
@@ -174,7 +174,21 @@ impl_number_prompt!(FloatPrompt, Float, f64);
 
 macro_rules! impl_ask {
     ($t:ident, $prompt_name:ident) => {
-        impl $t<'_> {
+        impl<'n> $t<'n> {
+            fn into_prompt<'a>(
+                self,
+                message: &'a str,
+                answers: &'a Answers,
+            ) -> $prompt_name<'n, 'a> {
+                $prompt_name {
+                    prompt: widgets::Prompt::new(message)
+                        .with_optional_hint(self.default.as_ref().map(ToString::to_string)),
+                    input: widgets::StringInput::new(Self::filter_map_char),
+                    number: self,
+                    answers,
+                }
+            }
+
             pub(crate) fn ask<B: Backend, E: Iterator<Item = error::Result<KeyEvent>>>(
                 mut self,
                 message: String,
@@ -184,17 +198,7 @@ macro_rules! impl_ask {
             ) -> error::Result<Answer> {
                 let transform = self.transform.take();
 
-                let ans = ui::Input::new(
-                    $prompt_name {
-                        prompt: widgets::Prompt::new(&*message)
-                            .with_optional_hint(self.default.as_ref().map(ToString::to_string)),
-                        input: widgets::StringInput::new(Self::filter_map_char),
-                        number: self,
-                        answers,
-                    },
-                    b,
-                )
-                .run(events)?;
+                let ans = ui::Input::new(self.into_prompt(&message, answers), b).run(events)?;
 
                 crate::write_final!(transform, message, ans, answers, b, Self::write(ans, b)?);
 
@@ -247,3 +251,108 @@ macro_rules! builder {
 
 builder!(IntBuilder, Int, i64, super::QuestionKind::Int);
 builder!(FloatBuilder, Float, f64, super::QuestionKind::Float);
+
+macro_rules! test_numbers {
+    (mod $mod_name:ident { $prompt_name:ident, $default:expr }) => {
+        #[cfg(test)]
+        mod $mod_name {
+            use ui::{backend::TestBackend, layout::Layout};
+
+            use super::*;
+
+            #[test]
+            fn test_render() {
+                let size = (50, 20).into();
+                let base_layout = Layout::new(5, size);
+                let answers = Answers::default();
+
+                let defaults = [(None, 17), (Some($default), 21)];
+
+                let mut backend = TestBackend::new_with_layout(size, base_layout);
+
+                for &(default, line_offset) in defaults.iter() {
+                    let mut prompt = $prompt_name {
+                        default,
+                        ..Default::default()
+                    }
+                    .into_prompt("message", &answers);
+
+                    let base_name = default.map(|_| "default").unwrap_or("no_default");
+
+                    let mut layout = base_layout;
+                    backend.reset_with_layout(layout);
+                    assert!(prompt.render(&mut layout, &mut backend).is_ok());
+                    assert_eq!(layout, base_layout.with_line_offset(line_offset));
+                    ui::assert_backend_snapshot!(format!("{}-1", base_name), backend);
+
+                    prompt.input.set_value("3".repeat(50));
+
+                    layout = base_layout;
+                    backend.reset_with_layout(layout);
+                    assert!(prompt.render(&mut layout, &mut backend).is_ok());
+                    assert_eq!(
+                        layout,
+                        base_layout.with_offset(0, 1).with_line_offset(line_offset)
+                    );
+                    ui::assert_backend_snapshot!(format!("{}-2", base_name), backend);
+                }
+            }
+
+            #[test]
+            fn test_height() {
+                let size = (50, 20).into();
+                let base_layout = Layout::new(5, size);
+                let answers = Answers::default();
+
+                let defaults = [(None, 17), (Some($default), 21)];
+
+                for &(default, line_offset) in defaults.iter() {
+                    let mut prompt = $prompt_name {
+                        default,
+                        ..Default::default()
+                    }
+                    .into_prompt("message", &answers);
+
+                    let mut layout = base_layout;
+
+                    assert_eq!(prompt.height(&mut layout), 1);
+                    assert_eq!(layout, base_layout.with_line_offset(line_offset));
+                    layout = base_layout;
+
+                    prompt.input.set_value("3".repeat(50));
+                    assert_eq!(prompt.height(&mut layout), 2);
+                    assert_eq!(
+                        layout,
+                        base_layout.with_offset(0, 1).with_line_offset(line_offset)
+                    );
+                }
+            }
+
+            #[test]
+            fn test_cursor_pos() {
+                let size = (50, 20).into();
+                let layout = Layout::new(5, size);
+                let answers = Answers::default();
+
+                let defaults = [(None, 17), (Some($default), 21)];
+
+                for &(default, line_offset) in defaults.iter() {
+                    let mut prompt = $prompt_name {
+                        default,
+                        ..Default::default()
+                    }
+                    .into_prompt("message", &answers);
+
+                    assert_eq!(prompt.cursor_pos(layout), (line_offset, 0));
+
+                    prompt.input.set_value("3".repeat(50));
+                    prompt.input.set_at(50);
+                    assert_eq!(prompt.cursor_pos(layout), (line_offset, 1));
+                }
+            }
+        }
+    };
+}
+
+test_numbers!(mod int_tests { Int, 333 });
+test_numbers!(mod float_tests { Float, 3.3 });
---
+++ tests/float.rs
@@ -0,0 +1,99 @@
+use discourse::{Answer, Question};
+use ui::{
+    events::{KeyCode, TestEvents},
+    style::Stylize,
+};
+
+mod helpers;
+
+#[test]
+fn test_validate() {
+    let prompt = Question::float("name").message("message").validate(|i, _| {
+        if i > 3.0 {
+            Ok(())
+        } else {
+            Err("The number must be more than 3".into())
+        }
+    });
+
+    let mut backend = helpers::SnapshotOnFlushBackend::new((50, 20).into());
+    let mut events = TestEvents::new(vec![
+        KeyCode::Enter.into(),
+        KeyCode::Char('2').into(),
+        KeyCode::Enter.into(),
+        KeyCode::Char('-').into(),
+        KeyCode::Enter.into(),
+        KeyCode::Backspace.into(),
+        KeyCode::Home.into(),
+        KeyCode::Char('3').into(),
+        KeyCode::Char('.').into(),
+        KeyCode::Enter.into(),
+    ]);
+
+    let ans = discourse::prompt_one_with(prompt, &mut backend, &mut events).unwrap();
+    assert_eq!(ans, Answer::Float(3.2));
+    assert!(events.next().is_none());
+}
+
+#[test]
+fn test_filter() {
+    let prompt = Question::float("name")
+        .message("message")
+        .filter(|i, _| i + 1.0);
+
+    let mut backend = helpers::SnapshotOnFlushBackend::new((50, 20).into());
+    let mut events = TestEvents::new(vec![
+        KeyCode::Char('2').into(),
+        KeyCode::Char('.').into(),
+        KeyCode::Char('2').into(),
+        KeyCode::Enter.into(),
+    ]);
+
+    let ans = discourse::prompt_one_with(prompt, &mut backend, &mut events).unwrap();
+    assert_eq!(ans, Answer::Float(3.2));
+    assert!(events.next().is_none());
+}
+
+#[test]
+fn test_transform() {
+    let prompt = Question::float("name")
+        .message("message")
+        .transform(|i, _, b| b.write_styled(&i.magenta()));
+
+    let mut backend = helpers::SnapshotOnFlushBackend::new((50, 20).into());
+    let mut events = TestEvents::new(vec![
+        KeyCode::Char('3').into(),
+        KeyCode::Char('.').into(),
+        KeyCode::Char('2').into(),
+        KeyCode::Enter.into(),
+    ]);
+
+    let ans = discourse::prompt_one_with(prompt, &mut backend, &mut events).unwrap();
+    assert_eq!(ans, Answer::Float(3.2));
+    assert!(events.next().is_none());
+}
+
+#[test]
+fn test_default() {
+    let prompt = Question::float("name").message("message").default(3.2);
+
+    let mut backend = helpers::SnapshotOnFlushBackend::new((50, 20).into());
+    let mut events = TestEvents::new(vec![
+        KeyCode::Char('3').into(),
+        KeyCode::Backspace.into(),
+        KeyCode::Enter.into(),
+    ]);
+
+    let ans = discourse::prompt_one_with(prompt, &mut backend, &mut events).unwrap();
+    assert_eq!(ans, Answer::Float(3.2));
+    assert!(events.next().is_none());
+
+    let prompt = Question::float("name").message("message").default(3.2);
+
+    let mut backend = helpers::SnapshotOnFlushBackend::new((50, 20).into());
+    let mut events = TestEvents::new(Some(KeyCode::Enter.into()));
+
+    let ans = discourse::prompt_one_with(prompt, &mut backend, &mut events).unwrap();
+    assert_eq!(ans, Answer::Float(3.2));
+    assert!(events.next().is_none());
+}
