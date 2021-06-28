68c5c214f4376c3e5ded0c84f80d621c4e22fdff
Lutetium-Vanadium <luv.s7000@gmail.com>
2021-06-28 14:14:03 +0530

    added tests for select

--- src/question/select.rs
+++ src/question/select.rs
@@ -110,7 +110,19 @@ impl widgets::List for Select<'_> {
     }
 }
 
-impl Select<'_> {
+impl<'a> Select<'a> {
+    fn into_prompt(self, message: &'a str) -> SelectPrompt<'a> {
+        let mut select = widgets::Select::new(self);
+        if let Some(default) = select.list.choices.default() {
+            select.set_at(default);
+        }
+
+        SelectPrompt {
+            prompt: widgets::Prompt::new(message),
+            select,
+        }
+    }
+
     pub(crate) fn ask<B: Backend, E: Iterator<Item = error::Result<KeyEvent>>>(
         mut self,
         message: String,
@@ -119,19 +131,9 @@ impl Select<'_> {
         events: &mut E,
     ) -> error::Result<Answer> {
         let transform = self.transform.take();
-        let mut select = widgets::Select::new(self);
-        if let Some(default) = select.list.choices.default() {
-            select.set_at(default);
-        }
-        let ans = ui::Input::new(
-            SelectPrompt {
-                prompt: widgets::Prompt::new(&message),
-                select,
-            },
-            b,
-        )
-        .hide_cursor()
-        .run(events)?;
+        let ans = ui::Input::new(self.into_prompt(&message), b)
+            .hide_cursor()
+            .run(events)?;
 
         crate::write_final!(
             transform,
@@ -223,4 +225,138 @@ impl<'a> From<SelectBuilder<'a>> for super::Question<'a> {
     fn from(builder: SelectBuilder<'a>) -> Self {
         builder.build()
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::prelude::*;
+    use rand_chacha::ChaCha12Rng;
+    use ui::{backend::TestBackend, events::KeyCode, layout::Layout};
+
+    use crate::question::{Choice, Question, QuestionKind};
+
+    use super::*;
+
+    const SEED: u64 = 9828123;
+    const SEP_RATIO: f32 = 0.3;
+    const DEFAULT_SEP_RATIO: f32 = 0.10;
+
+    fn choices(len: usize) -> impl Iterator<Item = Choice<String>> {
+        let mut rng = ChaCha12Rng::seed_from_u64(SEED);
+
+        (0..len).map(move |i| {
+            let rand: f32 = rng.gen();
+            if rand < DEFAULT_SEP_RATIO {
+                Choice::DefaultSeparator
+            } else if rand < SEP_RATIO {
+                Choice::Separator(format!("Separator {}", i))
+            } else {
+                Choice::Choice(format!("Choice {}", i))
+            }
+        })
+    }
+
+    fn unwrap_select<'a>(question: impl Into<Question<'a>>) -> Select<'a> {
+        match question.into().kind {
+            QuestionKind::Select(s) => s,
+            _ => unreachable!(),
+        }
+    }
+
+    macro_rules! test_select {
+        ($mod_name:ident { select = $select:expr; height = $height:expr $(;)? }) => {
+            test_select!($mod_name {
+                select = $select;
+                height = $height;
+                events = [
+                    KeyEvent::from(KeyCode::PageDown),
+                    KeyCode::Up.into(),
+                ];
+            });
+        };
+
+        ($mod_name:ident { select = $select:expr; height = $height:expr; events = $events:expr $(;)? }) => {
+            mod $mod_name {
+                use super::*;
+
+                #[test]
+                fn test_height() {
+                    let size = (50, 20).into();
+                    let base_layout = Layout::new(5, size);
+                    let mut select = $select.into_prompt("message");
+
+                    let events = $events;
+
+                    for &key in events.iter() {
+                        let mut layout = base_layout;
+
+                        assert_eq!(select.height(&mut layout), $height);
+                        assert_eq!(
+                            layout,
+                            base_layout.with_offset(0, $height).with_line_offset(0)
+                        );
+
+                        assert!(select.handle_key(key))
+                    }
+
+                    let mut layout = base_layout;
+
+                    assert_eq!(select.height(&mut layout), $height);
+                    assert_eq!(
+                        layout,
+                        base_layout.with_offset(0, $height).with_line_offset(0)
+                    );
+                }
+
+                #[test]
+                fn test_render() {
+                    let size = (50, 20).into();
+                    let base_layout = Layout::new(5, size);
+                    let mut select = $select.into_prompt("message");
+
+                    let mut backend = TestBackend::new(size);
+
+                    let events = $events;
+
+                    for &key in events.iter() {
+                        let mut layout = base_layout;
+                        backend.reset_with_layout(layout);
+
+                        assert!(select.render(&mut layout, &mut backend).is_ok());
+                        assert_eq!(
+                            layout,
+                            base_layout.with_offset(0, $height).with_line_offset(0)
+                        );
+                        ui::assert_backend_snapshot!(backend);
+
+                        assert!(select.handle_key(key))
+                    }
+
+                    let mut layout = base_layout;
+                    backend.reset_with_layout(layout);
+
+                    assert!(select.render(&mut layout, &mut backend).is_ok());
+                    assert_eq!(
+                        layout,
+                        base_layout.with_offset(0, $height).with_line_offset(0)
+                    );
+                    ui::assert_backend_snapshot!(backend);
+                }
+            }
+        };
+    }
+
+    test_select!(basic {
+        select = unwrap_select(
+                SelectBuilder::new("name".into()).choices(choices(10)),
+            );
+        height = 11;
+    });
+
+    test_select!(pagination {
+        select = unwrap_select(
+                SelectBuilder::new("name".into()).choices(choices(20)).default(6),
+            );
+        height = 16;
+    });
 }
---
+++ tests/select.rs
@@ -0,0 +1,53 @@
+use discourse::question::Choice;
+use rand::prelude::*;
+use rand_chacha::ChaCha12Rng;
+use ui::events::{KeyCode, KeyEvent, TestEvents};
+
+mod helpers;
+
+const SEED: u64 = 9828123;
+const SEP_RATIO: f32 = 0.3;
+const DEFAULT_SEP_RATIO: f32 = 0.10;
+
+fn choices(len: usize) -> impl Iterator<Item = Choice<String>> {
+    let mut rng = ChaCha12Rng::seed_from_u64(SEED);
+
+    (0..len).map(move |i| {
+        let rand: f32 = rng.gen();
+        if rand < DEFAULT_SEP_RATIO {
+            Choice::DefaultSeparator
+        } else if rand < SEP_RATIO {
+            Choice::Separator(format!("Separator {}", i))
+        } else {
+            Choice::Choice(format!("Choice {}", i))
+        }
+    })
+}
+
+#[test]
+fn test_transform() {
+    let checkbox = discourse::Question::select("name")
+        .transform(|item, _, b| {
+            b.set_fg(ui::style::Color::Magenta)?;
+            write!(b, "{}: {}", item.index, item.name)?;
+            b.set_fg(ui::style::Color::Reset)
+        })
+        .message("checkbox")
+        .choices(choices(10));
+
+    let size = (50, 20).into();
+    let mut backend = helpers::SnapshotOnFlushBackend::new(size);
+    let mut events = TestEvents::new(vec![
+        KeyEvent::from(KeyCode::PageDown),
+        KeyEvent::from(KeyCode::Up),
+        KeyCode::Enter.into(),
+    ]);
+
+    let ans = discourse::prompt_one_with(checkbox, &mut backend, &mut events)
+        .unwrap()
+        .try_into_list_item()
+        .unwrap();
+
+    assert!(events.next().is_none());
+    assert_eq!(ans.index, 8);
+}
