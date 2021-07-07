57fce982bfcd82e510d35dfe4ceb8a2c225816e6
Lutetium-Vanadium <luv.s7000@gmail.com>
2021-07-07 14:11:04 +0530

    Added mapping helper to convert ErrorKind to io::Error

--- discourse-ui/src/error.rs
+++ discourse-ui/src/error.rs
@@ -14,6 +14,19 @@ pub enum ErrorKind {
     Eof,
 }
 
+impl ErrorKind {
+    /// Maps `ErrorKind::Interrupted` or `ErrorKind::Eof` to `std::io::Error`.
+    ///
+    /// The function is passed `true` if it was Interrupted and `false` if EOF was received.
+    pub fn map_terminated<F: FnOnce(bool) -> io::Error>(self, f: F) -> io::Error {
+        match self {
+            ErrorKind::IoError(e) => e,
+            ErrorKind::Interrupted => f(true),
+            ErrorKind::Eof => f(false),
+        }
+    }
+}
+
 impl std::error::Error for ErrorKind {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
